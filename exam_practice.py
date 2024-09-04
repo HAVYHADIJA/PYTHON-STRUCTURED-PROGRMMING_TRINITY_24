9bf32f403d94de309824d7e821deebfd5cae1ac4
HAVYHADIJA <hnakalyowa29@gmail.com>
2024-09-04 14:43:32 +0300

    Add files via upload
    This was me practicing for python

---
+++ exam_practice.py
@@ -0,0 +1,46 @@
+# print("Hello world")
+# #this is a comment
+# #print a number
+# print(25)
+# #variables
+# name = "Hadjah"
+# number = 10
+# age = 12
+# name = "john"
+# #printing variables
+# print(name)
+# print(age)
+# print(name)
+# #data types
+# num = 2.3
+# print("The data type is", type(num))
+# answer = True
+# print("the data type is",type(answer))
+
+# #casting and data types
+# num_str = "12"
+# num = 15
+# print("the data type of is", type(num))
+# num_str = float(num_str)
+# print("The data type is",type(num_str))
+# sum = num + num_str
+# print(sum)
+# print("the data type of sum is",type(sum))
+
+print("Good morning",end="\t")
+print("Good evening")
+print("Soap","Sugar","Rice",sep="\n")
+#concactination
+print("Soap" + "sugar")
+print("this list has two items:","soap","sugar")
+#operators
+x = 14
+y= 8
+print (4*(14 +8*2))
+print(14%8 +2)
+
+#f string
+chairs = 10
+print(f"I am going to buy{chairs} chair covers")
+
+
